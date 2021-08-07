5c07b71928af786c242b2cec8bdd7b5bfd32aea1
springning <ustcnsp@gmail.com>
2021-08-07 16:25:04 +0800

    add java

---
+++ java-test/src/main/java/com/nsp/test/App.java
@@ -0,0 +1,13 @@
+package com.nsp.test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
